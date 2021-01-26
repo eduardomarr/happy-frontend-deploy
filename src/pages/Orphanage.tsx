72ea3b09e4e7f080e124764aceb24c91d4ce4b95
Eduardo Marques <dado.nep@gmail.com>
2021-01-26 09:00:13 -0300

    frontend-deploy

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -92,7 +92,7 @@ export default function Orphanage() {
               </MapContainer>
 
               <footer>
-              <a target="_blank" rel="noopener noreferrer" href={`https://google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
+              <a target="_blank" rel="noopener noreferrer" href={`https://google.com/maps/dir/?api=1&destination=${orphanageData.latitude},${orphanageData.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
